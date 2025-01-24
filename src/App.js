c9da97e8902b3e55af82fc32c4cd637f210a94ce
codefunk69 <voishn@icloud.com>
2025-01-24 22:55:08 +0100

    Create Home page

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <Home />
+        <section></section>
+        <footer></footer>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,25 @@
+import styles from "../Header/Header.module.css";
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <div>
+        <p className={styles.logo}>DANCEZILLA</p>
+      </div>
+      <nav>
+        <ul className={styles.navList}>
+          <li className={styles.navItem}>
+            <a href="#about">About</a>
+          </li>
+          <li className={styles.navItem}>
+            <a href="#gallery">Gallery</a>
+          </li>
+          <li className={styles.navItem}>
+            <a href="#faq">FAQ</a>
+          </li>
+        </ul>
+      </nav>
+      <button className={styles.buttonGetApp}>Get App</button>
+    </header>
+  );
+}
