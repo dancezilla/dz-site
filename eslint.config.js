47fd46c0a787af910e81637dbe645704ece68d52
ekolesnyk <eugene.kolesnyk@customertimes.com>
2025-02-16 19:23:19 +0100

    feat: implemented prettier

--- eslint.config.js
+++ eslint.config.js
@@ -3,8 +3,12 @@ import globals from "globals";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
-export default [
+/**
+ * @type {import("eslint").Linter.Config[]}
+ */
+const config = [
   { ignores: ["dist"] },
   {
     files: ["**/*.{js,jsx}"],
@@ -29,11 +33,24 @@ export default [
       ...react.configs["jsx-runtime"].rules,
       ...reactHooks.configs.recommended.rules,
       "react/jsx-no-target-blank": "off",
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
+      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
       "react/prop-types": "off",
+      "prettier/prettier": [
+        "error",
+        {
+          trailingComma: "es5",
+          tabWidth: 2,
+          semi: true,
+          singleQuote: false,
+          jsxSingleQuote: true,
+          bracketSpacing: true,
+          arrowParens: "avoid",
+          printWidth: 120,
+        },
+      ],
     },
   },
+  eslintPluginPrettierRecommended,
 ];
+
+export default config;
---
+++ prettier.config.js
@@ -0,0 +1,16 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  jsxSingleQuote: true,
+  bracketSpacing: true,
+  arrowParens: "avoid",
+  printWidth: 120,
+};
+
+export default config;
