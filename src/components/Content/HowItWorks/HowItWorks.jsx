d414fb3016f8ba4166e13a29b0c42926e6c782ec
codefunk69 <voishn@icloud.com>
2025-04-28 13:53:03 +0200

    grid structure

--- src/components/Content/HowItWorks/HowItWorks.jsx
+++ src/components/Content/HowItWorks/HowItWorks.jsx
@@ -2,22 +2,26 @@ import styles from "./HowItWorks.module.scss";
 
 const blocks = [
   {
+    img: "/steps-img/first.png",
     step: "Step 1:",
     title: "Create a Competition",
     description:
       "Open the app, set the parameters, and add participants. It’s intuitive — you’ll get it done in moments.",
   },
   {
+    img: "/slider-screen/Screen-2.png",
     step: "Step 2:",
     title: "Start Judging",
     description: "With a single tap, select the competition and dive into the evaluation process.",
   },
   {
+    img: "/slider-screen/Screen-3.png",
     step: "Step 3:",
     title: "Score Effortlessly",
     description: "One tap, and the scores are locked in. The app takes care of all the calculations for you.",
   },
   {
+    img: "/slider-screen/Screen-4.png",
     step: "Step 4:",
     title: "Get Instant Results",
     description: "Final scores and rankings appear immediately. Fast. Accurate. Flawless.",
@@ -30,9 +34,12 @@ export function HowItWorks() {
       <div className={styles.blocks}>
         {blocks.map((block, i) => (
           <div key={i} className={`${styles.block} `}>
-            <p className={styles.step}>{block.step}</p>
-            <h2 className={styles.title}>{block.title}</h2>
-            <p className={styles.description}>{block.description}</p>
+            <img src={block.img} />
+            <div className={styles.blockContent}>
+              <p className={styles.step}>{block.step}</p>
+              <h2 className={styles.title}>{block.title}</h2>
+              <p className={styles.description}>{block.description}</p>
+            </div>
           </div>
         ))}
       </div>
