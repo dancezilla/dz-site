ee3684d7c0f265a94ad5c6c8beb871c2dd80b138
codefunk69 <voishn@icloud.com>
2025-06-12 21:12:14 +0200

    fix: improve responsive layout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,19 +12,35 @@ export function Header() {
             <a href='#about'>About</a>
           </li>
           <li className={styles.navItem}>
-            <a href='#pricing'>Pricing</a>
+            <a href='#overview'>Overview </a>
           </li>
-          <li className={styles.navItem}>
+          {/* <li className={styles.navItem}>
             <a href='#gallery'>Gallery</a>
-          </li>
-          <li className={styles.navItem}>
-            <a href='#docs'>Docs</a>
-          </li>
+          </li> */}
+          {/* <li className={styles.navItem}>
+            <a>Docs</a>
+          </li> */}
         </ul>
       </nav>
       <div className={styles.buttonsSection}>
-        <button className={styles.buttons}>GET APP</button>
-        <button className={styles.buttons}>SIGN IT</button>
+        <button
+          onClick={() =>
+            (window.location.href =
+              "mailto:zntu1995@gmail.com?subject=Interested%20in%20working%20with%20us%3F%20Share%20your%20request%20and%20contact%20details%20%E2%80%94%20we%E2%80%99ll%20get%20in%20touch%20soon.")
+          }
+          className={styles.buttons}
+        >
+          GET APP
+        </button>
+        {/* <button
+          onClick={() =>
+            (window.location.href =
+              "mailto:zntu1995@gmail.com?subject=Interested%20in%20working%20with%20us%3F%20Share%20your%20request%20and%20contact%20details%20%E2%80%94%20we%E2%80%99ll%20get%20in%20touch%20soon.")
+          }
+          className={styles.buttons}
+        >
+          SIGN IT
+        </button> */}
       </div>
     </header>
   );
