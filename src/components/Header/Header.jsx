9e5cf7f73bd912e29b93a83b3588ec9019756868
codefunk69 <voishn@icloud.com>
2025-06-12 23:30:02 +0200

    feat: added mailto content

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,11 @@
 import styles from "./Header.module.scss";
 
 export function Header() {
+  const subject = encodeURIComponent("Request for demo");
+  const body = encodeURIComponent(
+    "Hello,\n\nI would like to try the app. Please let me know how I can get access or schedule a demo.\n\nBest regards,"
+  );
+
   return (
     <header className={styles.header}>
       <div>
@@ -24,10 +29,7 @@ export function Header() {
       </nav>
       <div className={styles.buttonsSection}>
         <button
-          onClick={() =>
-            (window.location.href =
-              "mailto:zntu1995@gmail.com?subject=Interested%20in%20working%20with%20us%3F%20Share%20your%20request%20and%20contact%20details%20%E2%80%94%20we%E2%80%99ll%20get%20in%20touch%20soon.")
-          }
+          onClick={() => (window.location.href = `mailto:zntu1995@gmail.com?subject=${subject}&body=${body}`)}
           className={styles.buttons}
         >
           GET APP
