6bc529f249f5ef9242ac297dd1fea5230f6fd724
codefunk69 <voishn@icloud.com>
2025-06-12 22:38:43 +0200

    fix: fixed build issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Home } from "@/components/Home/Home";
 // import { Gallery } from "@/components/Gallery/Gallery";
-import { About } from "./components/content/About/About";
-import { HowItWorks } from "./components/content/HowItWorks/HowItWorks";
+import { About } from "./components/Content/About/About";
+import { HowItWorks } from "./components/Content/HowItWorks/HowItWorks";
 import { Footer } from "./components/Footer/Footer";
 import "@/App.scss";
 
